d79400d191bf656c60782399c223d9c0b2e3f992
Jean-M. <146263595+EaglesView@users.noreply.github.com>
2024-05-30 11:20:12 -0400

    updated layout for eventual card display

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,14 @@
-import {Card} from "../ui/Card"
+import { Card } from "../ui/Card"
+import { fetchAllNotes } from "../lib/data"
+function DisplayCards() {
+    return <>
+
+    </>
+}
 export default function dashboard() {
     return <>
         <h1 className="text-3xl drop-shadow-md text-slate-800">My dashboard</h1>
-        <Card content={<></>} height={"h-full"} width={"w-full"} color={"bg-blue-200"}/>
+        <Card content={<></>} height={"h-full"} width={"w-full"} color={"bg-blue-200"} />
 
     </>
 }
---
+++ app/lib/data.tsx
@@ -0,0 +1,27 @@
+import prisma from "@/app/lib/prisma"
+import { GetServerSideProps } from "next";
+export const fetchAllNotes: GetServerSideProps = async ({ params }) => {
+    const notes = await prisma.notes.findMany({
+        where: {
+            authorID: String(params?.id),
+        },
+    });
+    return {
+        props: notes,
+    };
+};
+export const fetchNotesByTag: GetServerSideProps = async ({ params }) => {
+    const notes = await prisma.notes.findMany({
+        where: {
+            authorID: String(params?.id),
+        },
+        include: {
+            tags: {
+                select: { tag_name: true },
+            },
+        },
+    });
+    return {
+        props: notes,
+    };
+};
