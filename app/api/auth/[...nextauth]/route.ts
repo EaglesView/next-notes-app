fef42a31f40fb6bcea1e3090485c1b43ecd2771f
Jean-M. <146263595+EaglesView@users.noreply.github.com>
2024-05-31 09:45:05 -0400

    fix route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import NextAuth from 'next-auth';
 import options from './options';
+import { NextRequest, NextResponse } from 'next/server';
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => NextAuth(options)(req, res);
+const handler = (req: NextRequest, res: NextResponse) => NextAuth(options)(req, res);
 
 export { handler as GET, handler as POST };
