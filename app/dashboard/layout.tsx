c189ea727d756dfb931208ed1a50fdc368983249
Jean-M. <146263595+EaglesView@users.noreply.github.com>
2024-05-31 10:50:04 -0400

    hotilicious fix # 1

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -11,7 +11,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}
         </div>
       </div>
-      <RoundPlusButton />
     </>
   );
 }
