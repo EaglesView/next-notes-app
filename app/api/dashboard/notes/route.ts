e89c61df3516bb5e4bcc38f59f342df16671b8fc
Jean-M. <146263595+EaglesView@users.noreply.github.com>
2024-05-31 13:43:19 -0400

    updated

--- app/api/dashboard/notes/route.ts
+++ app/api/dashboard/notes/route.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../../../lib/prisma';
 
-const Handler = async (req: NextApiRequest, res: NextApiResponse) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'GET') {
         try {
             const { authorID } = req.query;
@@ -44,4 +44,4 @@ const Handler = async (req: NextApiRequest, res: NextApiResponse) => {
     }
 };
 
-export default Handler;
+export { handler as GET, handler as POST }
